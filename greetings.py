628d12983974772a070b8ee5bcb432de4065af41
zeggaisamira-ui <zeggai.samira@gmail.com>
2026-02-09 16:01:29 +0100

    Ajout du fichier greetings.py

---
+++ greetings.py
@@ -0,0 +1,4 @@
+print("Je suis en train d'apprendre à utiliser Git et GitHub et j'adore ça !")
+nom = input("Quel est ton nom ? ")
+print(f"Enchanté, {nom} ! Passe une bonne journée en codant avec Git et GitHub.")
+
